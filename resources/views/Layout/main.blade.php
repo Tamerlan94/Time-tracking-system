c3465c2c8814705c5e9f3815d0bb0dd1ab75013b
Tamerlan <mctamer94@gmail.com>
2022-07-18 21:59:08 +0600

    обавлены js файлы

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -6,6 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap-datepicker.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
     <title>Main</title>
 </head>
 <body>
@@ -13,5 +16,6 @@
     @yield('content')
 </div>
 <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
 </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,8 @@
             <div class="p-2 text-center">
                 <img
                     src="https://advancingdatajustice.turing.ac.uk/assets/decidim/default-avatar-43686fd5db4beed0141662a012321bbccd154ee1d9188b0d1f41e37b710af3cb.svg"
-                    class="img-fluid" alt="avatar" style="width: 100px">
-                <select class="form-select form-select-sm" disabled>
+                    class="img-fluid" alt="avatar" style="width: 130px">
+                <select name="" class="form-select form-select-sm" disabled>
                     <option value="1">Админ</option>
                     <option value="2">Менеджер</option>
                     <option selected value="3">Пользователь</option>
@@ -17,7 +17,21 @@
                 <h1>Учет рабочего времени</h1>
             </div>
         </div>
-
+        <hr>
+        <div class="d-flex justify-content-between">
+            <button type="button" class="btn btn-secondary">Secondary</button>
+            <div class="d-flex">
+                <h3>Сегодня</h3>
+                <div class="input-group date" data-provide="datepicker">
+                    <input type="text" class="form-control">
+                    <div class="input-group-addon">
+                        <span class="glyphicon glyphicon-th"></span>
+                    </div>
+                </div>
+            </div>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
     </div>
-
 @endsection
