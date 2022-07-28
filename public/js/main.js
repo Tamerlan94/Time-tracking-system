f88cfaa26b1fcf229377477080025103a82a7e18
Tamerlan <mctamer94@gmail.com>
2022-07-28 21:32:35 +0600

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -5,14 +5,6 @@ $(document).ready(function () {
         todayBtn: "linked",
     });
 
-    $('.input-daterange input').each(function () {
-        $(this).datepicker('clearDates', {
-            format: "dd-mm-yyyy",
-            todayHighlight: true,
-            todayBtn: "linked",
-        });
-    });
-
     $('#timepicker1').timepicker();
     $('#timepicker2').timepicker();
 
@@ -21,60 +13,74 @@ $(document).ready(function () {
         element.appendTo('#searchPlace');
     }, 10);
 
-    let user_id;
-    let project_id;
-    let project_name;
-    let project_description;
-    let task_id;
-    let task_name;
-    let task_comment;
-    let task_time_from;
-    let task_time_to;
-    let status;
+    // $('.task-b').on("click", function (e) {
+        // let taskId = $(this).parent().children("td:first").text();
+        // console.log(taskId);
 
-    $('.task-b').on("click", function (e) {
-        let taskId = $(this).parent().children("td:first").text();
-        console.log(taskId);
-
-        $.ajax({
-            url: 'api/get-info',
-            method: 'post',
-            dataType: 'json',
-            data: {id: taskId},
-            success: function (e) {
-                console.log(e);
-                project_id = e.project_id;
-                task_id = e.id;
-                user_id = e.user_id;
-
-                status = $('#selectStatus').val();
-                task_name = $('.modal-header div h5').text(e.name);
-                project_name = $('.modal-header div h6').text(e.project.name);
-                project_description = $('.modal-body #desc').text(e.description);
-                task_comment = $('.modal-body #comment').text(e.work_hours[0].comment);
-            }
-        })
-
-        let modalWindow = new bootstrap.Modal($('#modal'));
-        // $('.modal-title').append('Задача');
-        modalWindow.show();
-        // console.log($(this).text());
-    });
-
-    $('#save').click(function (e) {
-        e.preventDefault();
-        $.ajax({
-            url: 'api/createOrUpdateTask',
-            method: 'post',
-            dataType: 'json',
-            data: {
-                project_id: project_id,
-                status_id: status,
-                user_id: user_id,
-                name: task_name,
-                description: project_description,
-            }
+        // $.ajax({
+        //     url: 'api/get-info',
+        //     method: 'post',
+        //     dataType: 'json',
+        //     data: {id: taskId},
+        //     success: function (e) {
+        //         console.log(e);
+        //         project_id = e.project_id;
+        //         task_id = e.id;
+        //         user_id = e.user_id;
+        //
+        //         status = $('#selectStatus').val();
+        //         task_name = $('.modal-header div h5').text(e.name);
+        //         project_name = $('.modal-header div h6').text(e.project.name);
+        //         project_description = $('.modal-body #desc').text(e.description);
+        //         task_comment = $('.modal-body #comment').text(e.work_hours[0].comment);
+        //     }
         });
-    });
 
+    //     let modalWindow = new bootstrap.Modal($('#modal'));
+    //     // $('.modal-title').append('Задача');
+    //     modalWindow.show();
+    //     // console.log($(this).text());
+    // });
+
+
+
+let maimApp = new Vue({
+    el: '#mainApp',
+    data: {
+        taskName: '',
+        taskProject: '',
+        taskDescription: '',
+        taskFrom: '',
+        taskTo: '',
+        taskStatuses: '',
+        taskComment: ''
+    },
+    mounted() {
+        $('.task-b').on("click", function (e) {
+            let taskId = $(this).parent().children("td:first").text();
+            this.taskInfo(taskId);
+        });
+    },
+    methods: {
+        saveTask() {
+
+        },
+        taskInfo(taskId) {
+            axios
+                .post('api/get-info', {
+                    id: taskId,
+                })
+                .then(response => {
+                    this.taskName = response.data.name;
+                    this.taskProject = response.data.project.name;
+                    this.taskDescription = response.data.description;
+                    this.taskComment = response.data.comment;
+                })
+
+            let modalWindow = new bootstrap.Modal($('#modal'));
+            // $('.modal-title').append('Задача');
+            modalWindow.show();
+        }
+
+    },
 });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,78 +1,79 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="border rounded-2">
-        <div class="d-flex">
-            <div class="card ms-3" style="width: 100px;">
-                <img
-                    src="https://advancingdatajustice.turing.ac.uk/assets/decidim/default-avatar-43686fd5db4beed0141662a012321bbccd154ee1d9188b0d1f41e37b710af3cb.svg"
-                    class="card-img-top" alt="avatar" style="width: 100px">
-                <div class="text-center p-1">
-                    <h5 class="card-title">{{ auth()->user()->role->name }}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{ auth()->user()->first_name }}</h6>
-                    <a class="btn btn btn-outline-secondary" href="/api/logout">выход</a>
-                </div>
-            </div>
-            <div class="text-center align-self-center flex-fill">
-                <h1>Учет рабочего времени</h1>
-            </div>
-        </div>
-        <hr class="m-2">
-        <div class="d-flex justify-content-between m-3">
-            <a href="/statistic" type="button" class="btn btn-secondary" id="bbb">Посмотреть персональную статистику</a>
+    <div id="mainApp">
+        <div class="border rounded-2">
             <div class="d-flex">
-                <h4 class="me-2">Сегодня: </h4>
-                <div class="input-group date">
-                    <input type="text" class="form-control datepicker" value="{{ date('d-m-Y') }}">
-                    <div class="input-group-text">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                             class="bi bi-calendar-check" viewBox="0 0 16 16">
-                            <path
-                                d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"></path>
-                            <path
-                                d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"></path>
-                        </svg>
+                <div class="card ms-3" style="width: 100px;">
+                    <img
+                        src="https://advancingdatajustice.turing.ac.uk/assets/decidim/default-avatar-43686fd5db4beed0141662a012321bbccd154ee1d9188b0d1f41e37b710af3cb.svg"
+                        class="card-img-top" alt="avatar" style="width: 100px">
+                    <div class="text-center p-1">
+                        <h5 class="card-title">{{ auth()->user()->role->name }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{ auth()->user()->first_name }}</h6>
+                        <a class="btn btn btn-outline-secondary" href="/api/logout">выход</a>
                     </div>
                 </div>
+                <div class="text-center align-self-center flex-fill">
+                    <h1>Учет рабочего времени</h1>
+                </div>
             </div>
-            <div class="d-flex" role="search" id="searchPlace">
+            <hr class="m-2">
+            <div class="d-flex justify-content-between m-3">
+                <a href="/statistic" type="button" class="btn btn-secondary" id="bbb">Посмотреть персональную статистику</a>
+                <div class="d-flex">
+                    <h4 class="me-2">Сегодня: </h4>
+                    <div class="input-group date">
+                        <input type="text" class="form-control datepicker" value="{{ date('d-m-Y') }}">
+                        <div class="input-group-text">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-calendar-check" viewBox="0 0 16 16">
+                                <path
+                                    d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"></path>
+                                <path
+                                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"></path>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex" role="search" id="searchPlace">
 
+                </div>
             </div>
-        </div>
-        <div>
-            <table class="table table-striped table-bordered" id="table" data-toggle="table" data-search="true"
-                   data-search-highlight="true">
-                <thead>
-                <tr>
-                    <th scope="col">№</th>
-                    <th scope="col" data-field="project" data-sortable="true" id="projectFilter">Проект</th>
-                    <th scope="col" data-field="task" data-searchable="true">Задача</th>
-                    <th scope="col" data-field="deadline" data-sortable="true" id="deadlineFilter">Дэдлайн</th>
-                    <th scope="col">07</th>
-                    <th scope="col">08</th>
-                    <th scope="col">09</th>
-                    <th scope="col">10</th>
-                    <th scope="col">11</th>
-                    <th scope="col">12</th>
-                    <th scope="col">13</th>
-                    <th scope="col">14</th>
-                    <th scope="col">15</th>
-                    <th scope="col">16</th>
-                    <th scope="col">17</th>
-                    <th scope="col">18</th>
-                    <th scope="col">19</th>
-                    <th scope="col">20</th>
-                    <th scope="col">21</th>
-                    <th scope="col">22</th>
-                </tr>
-                </thead>
-                <tbody class="table table-hover">
-                @foreach($tasks as $task)
+            <div>
+                <table class="table table-striped table-bordered" id="table" data-toggle="table" data-search="true"
+                       data-search-highlight="true">
+                    <thead>
                     <tr>
-                        <td>{{ $task->id }}</td>
-                        <td>{{ $task->project->name }}</td>
-                        <td class="task-b">{{ $task->name }}</td>
-                        <td class="
+                        <th scope="col">№</th>
+                        <th scope="col" data-field="project" data-sortable="true" id="projectFilter">Проект</th>
+                        <th scope="col" data-field="task" data-searchable="true">Задача</th>
+                        <th scope="col" data-field="deadline" data-sortable="true" id="deadlineFilter">Дэдлайн</th>
+                        <th scope="col">07</th>
+                        <th scope="col">08</th>
+                        <th scope="col">09</th>
+                        <th scope="col">10</th>
+                        <th scope="col">11</th>
+                        <th scope="col">12</th>
+                        <th scope="col">13</th>
+                        <th scope="col">14</th>
+                        <th scope="col">15</th>
+                        <th scope="col">16</th>
+                        <th scope="col">17</th>
+                        <th scope="col">18</th>
+                        <th scope="col">19</th>
+                        <th scope="col">20</th>
+                        <th scope="col">21</th>
+                        <th scope="col">22</th>
+                    </tr>
+                    </thead>
+                    <tbody class="table table-hover">
+                    @foreach($tasks as $task)
+                        <tr>
+                            <td>{{ $task->id }}</td>
+                            <td>{{ $task->project->name }}</td>
+                            <td class="task-b">{{ $task->name }}</td>
+                            <td class="
                         @php
                     $date = strtotime($task->deadline);
                     $currentDate = strtotime(date('d-m-Y'));
@@ -87,85 +88,86 @@
                         echo "table-alarm";
                     }
                     @endphp">{{ $task->deadline }}</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
 
 
-    {{--  Модальное окно  --}}
-    <div class="modal fade" id="modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-         aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <div>
-                        <h5 class="modal-title" id="staticBackdropLabel">Задача 1</h5>
-                        <h6>Проект 1</h6>
+        {{--  Модальное окно  --}}
+        <div class="modal fade" id="modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+             aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <div>
+                            <h5 class="modal-title" id="staticBackdropLabel">@{{ taskName }}</h5>
+                            <h6>@{{ taskProject }}</h6>
+                        </div>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
+                    <div class="modal-body">
                     <textarea @if(auth()->user()->role_id == 3) disabled @endif
-                    class="form-control mb-3" id="desc" rows="3" placeholder="Описание">Описание задачи</textarea>
-                    <div class="d-flex align-items-center mb-3">
-                        <p class="m-0 me-2">С</p>
-                        <div class="input-group bootstrap-timepicker timepicker">
-                            <input type="text" id="timepicker1" class="form-control">
-                            <div class="input-group-text input-group-addon">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                     class="bi bi-stopwatch" viewBox="0 0 16 16">
-                                    <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
-                                    <path
-                                        d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
-                                </svg>
+                    class="form-control mb-3" id="desc" rows="3" placeholder="Описание">@{{ taskDescription }}</textarea>
+                        <div class="d-flex align-items-center mb-3">
+                            <p class="m-0 me-2">С</p>
+                            <div class="input-group bootstrap-timepicker timepicker">
+                                <input type="text" id="timepicker1" class="form-control" v-model="taskFrom">
+                                <div class="input-group-text input-group-addon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         class="bi bi-stopwatch" viewBox="0 0 16 16">
+                                        <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+                                        <path
+                                            d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+                                    </svg>
+                                </div>
+                            </div>
+                            <p class="m-0 me-2">По</p>
+                            <div class="input-group bootstrap-timepicker timepicker">
+                                <input type="text" id="timepicker2" class="form-control" v-model="taskTo">
+                                <div class="input-group-text input-group-addon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         class="bi bi-stopwatch" viewBox="0 0 16 16">
+                                        <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+                                        <path
+                                            d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+                                    </svg>
+                                </div>
                             </div>
                         </div>
-                        <p class="m-0 me-2">По</p>
-                        <div class="input-group bootstrap-timepicker timepicker">
-                            <input type="text" id="timepicker2" class="form-control">
-                            <div class="input-group-text input-group-addon">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                     class="bi bi-stopwatch" viewBox="0 0 16 16">
-                                    <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
-                                    <path
-                                        d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
-                                </svg>
-                            </div>
-                        </div>
+                        <select class="form-select mb-3" id="selectStatus" v-model="taskStatuses">
+                            <option disabled selected>Тип работы</option>
+                            @foreach($statuses as $status)
+                                @if($status->role_id == auth()->user()->role_id)
+                                    <option value="{{ $status->id }}">{{ $status->name }}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                        <textarea class="form-control mb-3" id="comment" rows="3"
+                                  placeholder="Комментарий" v-model="taskComment">@{{ taskComment }}</textarea>
                     </div>
-                    <select class="form-select mb-3" id="selectStatus">
-                        <option disabled selected>Тип работы</option>
-                        @foreach($statuses as $status)
-                            @if($status->role_id == auth()->user()->role_id)
-                                <option value="{{ $status->id }}">{{ $status->name }}</option>
-                            @endif
-                        @endforeach
-                    </select>
-                    <textarea class="form-control mb-3" id="comment" rows="3"
-                              placeholder="Комментарий">Комментарий</textarea>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-success" id="save" data-bs-dismiss="modal">Сохранить</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-success" id="save" data-bs-dismiss="modal" v-on:click="saveTask">Сохранить</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                    </div>
                 </div>
             </div>
         </div>
